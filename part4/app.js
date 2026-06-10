b1196f81b836940dd6955767029060a79e31b4f9
JoGrace1 <Joana.noack@gmail.com>
2026-06-11 01:10:34 +0200

    5.15

--- part4/app.js
+++ part4/app.js
@@ -4,7 +4,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
-
+const usersRouter = require('./controllers/users')
 const app = express()
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -13,7 +13,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
-
+app.use('/api/users', usersRouter)
 // mongoose.connect(config.MONGODB_URI, { family: 4 })
 //   .then(() => logger.info('connected to MongoDB'))
 //   .catch(err => logger.error(err.message))
